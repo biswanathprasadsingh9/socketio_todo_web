d3003f9204fbf7c234825ab0ed24db0b6d15e155
BIswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-07-12 07:43:26 +0530

    '12Jul2021'

--- next.config.js
+++ next.config.js
@@ -7,8 +7,9 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
   env: {
+    backendURL: 'https://socketio-todo-server.vercel.app/api',
+    socket: 'https://socketio-todo-server.vercel.app'
     // backendURL: 'http://localhost:5000/api',
-    backendURL: 'http://localhost:5000/api',
-    socket: 'http://localhost:5000'
+    // socket: 'http://localhost:5000'
   }
 }
